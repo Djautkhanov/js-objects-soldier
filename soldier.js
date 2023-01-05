59011aaffd9f73a3b2e45fdabbd8e2a30540c7da
Djautkhanov <vakha.96@mail.ru>
2023-01-05 11:18:30 +0300

    Добавил методы

--- soldier.js
+++ soldier.js
@@ -6,7 +6,26 @@
         ammo : 30,
     },
     supplies : 3,
-    fire : function (){},
-    recharge : function (){},
-    hurt : function (){},
- }
+    fire : function (){
+        this.weapon.ammo -- ;
+        return console.log ('бах-бах')
+    },
+    recharge : function (){
+        if(this.supplies === 0){
+            return console.log('не осталось припасов')
+        }
+        this.weapon.ammo = 30 ;
+        this.supplies --;
+        return console.log ('перезарядка...')
+    },
+    hurt : function (){
+        this.health --;
+        if (this.health <= 0) {
+            return console.log ('Ты проиграл')
+    }
+ },
+}
+ soldier.hurt()
+ console.log (soldier.health);
+
+ 
