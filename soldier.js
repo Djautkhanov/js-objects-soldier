e09aa706fc3e6e7b8389016afe9318b1a8eff811
Djautkhanov <vakha.96@mail.ru>
2023-01-05 10:54:10 +0300

    Создал объект

---
+++ soldier.js
@@ -0,0 +1,12 @@
+ const soldier = {
+    firstName : 'John',
+    health : 10,
+    weapon : {
+        name : 'Ak -47',
+        ammo : 30,
+    },
+    supplies : 3,
+    fire : function (){},
+    recharge : function (){},
+    hurt : function (){},
+ }
